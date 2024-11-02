f44567d81c6e03920f9000fe33b23e0fe341f643
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-11-02 19:36:30 +0530

    Update blog.js

--- blog.js
+++ blog.js
@@ -4,15 +4,14 @@ const readMoreButtons = document.querySelectorAll('.read-more-btn');
 // Add a click event listener for each button
 readMoreButtons.forEach((button) => {
   button.addEventListener('click', function() {
-    // Find the content wrapper by going to the previous sibling
-    const contentWrapper = this.previousElementSibling;
-
+    const contentWrapper = this.previousElementSibling; // Select the previous sibling which is the content wrapper
+    
     // Toggle the 'expanded' class on the content wrapper
     if (contentWrapper.classList.contains('expanded')) {
-      contentWrapper.classList.remove('expanded');
+      contentWrapper.classList.remove('expanded'); // Collapse content
       this.textContent = 'Read More...'; // Change button text to 'Read More'
     } else {
-      contentWrapper.classList.add('expanded');
+      contentWrapper.classList.add('expanded'); // Expand content
       this.textContent = 'Show Less'; // Change button text to 'Show Less'
     }
   });
